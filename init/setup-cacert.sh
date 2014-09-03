6799421286fa40e5b81ebdb21bfff9e0f7748496
Christoph Keller <gri@not-censored.com>
2014-09-03 12:36:47 +0200

    Better check checksums as I do not trust the dns server :)

--- init/setup-cacert.sh
+++ init/setup-cacert.sh
@@ -4,6 +4,7 @@ cd /tmp
 
 function install_cert() {
 	CERT_URL=$1
+	CERT_HASH=$2
 	CERT_NAME=$(basename $CERT_URL)
 	CERT_PATH=/tmp/$CERT_NAME
 
@@ -11,6 +12,13 @@ function install_cert() {
 	
 	if [ $? -gt 0 ]; then
 		echo "Failed to download certificate from $CERT_URL"
+		return
+	fi
+	
+	# Do checksum tests
+	FILE_HASH=$(sha256sum $CERT_PATH | awk '{print $1;}')
+	if [ "$FILE_HASH" != "$CERT_HASH" ]; then
+		echo "Invalid sha256 sum for $CERT_NAME"
 		return
 	fi
 
@@ -21,7 +29,12 @@ function install_cert() {
 
 	# Install certificate in strongswan calist
 	sudo cp $CERT_PATH /usr/local/etc/ipsec.d/cacerts/
+	
+	# Remove the certificate from tmp
+	rm $CERT_PATH
 }
 
-install_cert https://www.cacert.org/certs/root.crt
-install_cert https://www.cacert.org/certs/class3.crt
+install_cert https://www.cacert.org/certs/root.crt \
+	c0e0773a79dceb622ef6410577c19c1e177fb2eb9c623a49340de3c9f1de2560
+install_cert https://www.cacert.org/certs/class3.crt \
+	f5badaa5da1cc05b110a9492455a2c2790d00c7175dcf3a7bcb5441af71bf84f
